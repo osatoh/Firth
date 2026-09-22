5c2d048e1bcd7344ecd3b61c8ee56f339ca3e549
osatoh <sugar961216@gmail.com>
2026-09-22 23:32:11 +0900

    feat: explain Firth on the landing page and rewrite the README

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,10 +2,11 @@ require "rails_helper"
 
 RSpec.describe "Pages", type: :request do
   describe "landing" do
-    it "renders without a sign-in" do
+    it "explains what Firth is without a sign-in" do
       get root_path
 
       expect(response).to have_http_status(:ok)
+      expect(response.body).to include(*I18n.t("pages.landing.features"))
     end
   end
 end
